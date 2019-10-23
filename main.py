6a852724b3cc12213d5bad0573f344082da23174
Juan Lopez <juan07alberto@gmail.com>
2019-10-23 18:54:26 +0100

    modified

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import RCWL
+import RPi.GPIO as GPIO
 import motionSensor
 import dht11
 import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup
 # instance = motionSensor.motionSensor(2)
 # x = 0;
 # while True:
@@ -9,15 +14,18 @@ import time
 #     xd = instance.returnResults()
 #     print("{}] {} ".format(x, xd))
 
-# instance = RCWL.rcwl(7)
+instance = RCWL.rcwl(12)
 
-# while True:
-#     read = instance.returnResults
-#     print(read+"\n")
+while True:
+     read = instance.returnResults
+     print(read)
 
-instance = dht11.DHT11(12)
+instance = dht11.DHT11(pin = 7)
 
 while True:
     result  = instance.read()
-    print("Temperatura : {}, Humedad : {}".format(result.temperature, result.humidity))
-    time.sleep(2)
+    if result.is_valid():
+        print("Temperatura : {}, Humedad : {}".format(result.temperature, result.humidity))
+        time.sleep(2)
+
+GPIO.cleanup()
