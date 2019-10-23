8aeeff71f095b34b6cddd8a96a0113a5cb467004
Juan Lopez Ix <juan07alberto@gmail.com>
2019-10-22 19:55:33 -0500

    test dht11

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import RCWL
 import motionSensor
-
+import dht11
+import time
 # instance = motionSensor.motionSensor(2)
 # x = 0;
 # while True:
@@ -8,8 +9,15 @@ import motionSensor
 #     xd = instance.returnResults()
 #     print("{}] {} ".format(x, xd))
 
-instance = RCWL.rcwl(7)
+# instance = RCWL.rcwl(7)
+
+# while True:
+#     read = instance.returnResults
+#     print(read+"\n")
+
+instance = dht11.DHT11(12)
 
 while True:
-    read = instance.returnResults
-    print(read+"\n")
+    result  = instance.read()
+    print("Temperatura : {}, Humedad : {}".format(result.temperature, result.humidity))
+    time.sleep(2)
